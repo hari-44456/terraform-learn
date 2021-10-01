9dd45ac1f3ab7383e6e8f67a2024a5b0dbae7952
hari-44456 <naraharipapshetwar@gmail.com>
2021-10-01 11:57:54 +0530

    added remote state

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+    required_version = ">=0.12"
+    backend "s3" {
+        bucket = "narahari-bucket"
+        key = "myapp/state.tfstate"
+        region = "ap-south-1"
+    }
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
